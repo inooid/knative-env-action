4288dfdd3c61d3884656dfb592d37b73d74fb798
Boyd Dames <boyd@hedge.video>
2024-10-10 12:45:46 +0200

    Add test for handling no containers

--- __tests__/knative.test.js
+++ __tests__/knative.test.js
@@ -2,6 +2,24 @@ const { expect } = require('@jest/globals')
 const { updateContainer } = require('../src/knative')
 
 describe('updateContainer()', () => {
+  it('throws when there are no containers', () => {
+    const manifest = {
+      apiVersion: 'serving.knative.dev/v1',
+      kind: 'Service',
+      spec: {
+        template: {
+          spec: {
+            containers: []
+          }
+        }
+      }
+    }
+
+    expect(() => updateContainer(manifest, 'foo', () => {})).toThrow(
+      `No containers found in 'spec.template.spec.containers'`
+    )
+  })
+
   it('throws when the given container cannot be found', () => {
     const manifest = {
       apiVersion: 'serving.knative.dev/v1',

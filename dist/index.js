5668ae185a433235206b56b0fc6fdc9c567fa198
Boyd Dames <boyd@hedge.video>
2024-10-10 11:12:40 +0200

    Update latest build

--- dist/index.js
+++ dist/index.js
@@ -3106,8 +3106,40 @@ async function parse(filePath) {
   return dotenv.parse(file)
 }
 
+/**
+ * Simplified version of the Linux native envsubst() method.
+ *
+ * Currently unsupported features:
+ * - Any fallbacks ${NOTSET:=$BAR}
+ * - Escaping using $$
+ * @param {string} input
+ * @param {object} env
+ * @returns {string}
+ */
+function envsubst(input, env = process.env) {
+  const varPattern =
+    /\$\{([a-zA-Z_][a-zA-Z0-9_]*)\}|\$([a-zA-Z_][a-zA-Z0-9_]*)/g
+
+  // Replace all instances of ${VAR} or $VAR with their corresponding environment values
+  const result = input.replace(
+    varPattern,
+    (_, bracketVariant, dollarVariant) => {
+      const varName = bracketVariant || dollarVariant
+
+      // When env does not exist, fallback to the original token ($FOO or ${FOO})
+      return (
+        env[varName] ??
+        (bracketVariant ? `\${${bracketVariant}}` : `$${dollarVariant}`)
+      )
+    }
+  )
+
+  return result
+}
+
 module.exports = {
-  parse
+  parse,
+  envsubst
 }
 
 
@@ -3118,15 +3150,17 @@ module.exports = {
 
 const fs = __nccwpck_require__(3977)
 const YAML = __nccwpck_require__(4083)
+const { envsubst } = __nccwpck_require__(4521)
 
 /**
+ * Reads the given knative manifest and replaces the given env vars.
  * @param {string} filePath
  * @returns {object}
  */
 async function readManifest(filePath) {
   const file = await fs.readFile(filePath, { encoding: 'utf8' })
 
-  return YAML.parse(file)
+  return YAML.parse(envsubst(file))
 }
 
 /**
